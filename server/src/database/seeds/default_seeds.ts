0a8207d98423a1cc4e056c4401724e197fa3382b
David Moreira <davidmoreira37@gmail.com>
2020-06-26 11:31:45 +0100

    Fix database seeds state descriptions

--- server/src/database/seeds/default_seeds.ts
+++ server/src/database/seeds/default_seeds.ts
@@ -3,7 +3,7 @@ import Knex from 'knex'
 
 export async function seed (knex: Knex) {
   await knex('states').insert([
-    { id: 0, state: 'active' },
-    { id: 1, state: 'inactive' }
+    { id: 0, state: 'inactive' },
+    { id: 1, state: 'active' }
   ])
 }
