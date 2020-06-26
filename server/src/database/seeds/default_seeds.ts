e81a985f4a5e05d2e32f7d55cb23ebba25959d97
David Moreira <davidmoreira37@gmail.com>
2020-06-26 08:18:54 +0100

    Reverte previous fix to enable eslint

--- server/src/database/seeds/default_seeds.ts
+++ server/src/database/seeds/default_seeds.ts
@@ -1,8 +1,8 @@
 import Knex from 'knex'
 
-export async function seed(knex: Knex) {
+export async function seed (knex: Knex) {
   await knex('states').insert([
     { id: 0, state: 'active' },
-    { id: 1, state: 'inactive' },
+    { id: 1, state: 'inactive' }
   ])
 }
