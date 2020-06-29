77346c8a3d790be4665ceaafac44e12bbf505976
David Moreira <davidmoreira37@gmail.com>
2020-06-29 21:07:23 +0100

    Add body validation to route create members

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { celebrate, Joi } from 'celebrate'
 
 import MemberController from '@controllers/MemberController'
 
@@ -8,7 +9,21 @@ const memberController = new MemberController()
 
 routes.get('/members', memberController.index)
 routes.get('/members/:id', memberController.show)
-routes.post('/members', memberController.create)
+routes.post('/members',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      address: Joi.string(),
+      phone: Joi.number(),
+      email: Joi.string().email(),
+      description: Joi.string(),
+      state: Joi.string().required()
+    })
+  }, {
+    abortEarly: false
+  }),
+  memberController.create
+)
 routes.put('/members/:id', memberController.update)
 
 export default routes
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { errors } from 'celebrate'
 
 import routes from './routes'
 import { PORT_SERVER } from '@config/config.json'
@@ -7,5 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use(errors())
 
 app.listen(PORT_SERVER)
