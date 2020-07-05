cb88f57329b814585d49f5f4e807246fad35ba59
David Moreira <davidmoreira37@gmail.com>
2020-07-05 08:23:08 +0100

    Remove unused interface

--- server/src/middleware/role.ts
+++ server/src/middleware/role.ts
@@ -1,12 +1,6 @@
 // eslint-disable-next-line
 import { Request, Response, NextFunction } from 'express'
 
-interface Decoded {
-  id: String
-  iat: String
-  exp: String
-}
-
 export default (request: Request, response: Response, next: NextFunction) => {
   const userId = Number(request.headers.user_id)
 
