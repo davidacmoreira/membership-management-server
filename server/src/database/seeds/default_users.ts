e02b0b51baeafbcbec2fdb9903f1ceeca054cd92
David Moreira <davidmoreira37@gmail.com>
2020-07-06 20:33:40 +0100

    Add database seeds for default users

--- server/src/database/seeds/default_users.ts
+++ server/src/database/seeds/default_users.ts
@@ -8,6 +8,12 @@ export async function seed (knex: Knex) {
       username: 'admin',
       password: '$2b$10$kJe/YWrU68mWrB2YfKgUPuWP722tjuyIf83lpzZp2xSHU02bggCiS',
       token: ''
+    },
+    {
+      id: 2,
+      username: 'user',
+      password: '$2b$10$8f4tusLV9uVR/qIXy7.1zu5EDD5th5RS779/lABXXBwifNu9R4tve',
+      token: ''
     }
   ])
 }
